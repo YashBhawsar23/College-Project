fb3b95c8887f4cbbb826fab17326d42b2f94d9ed
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-03-13 17:58:53 +0530

    Navbar fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,101 +1,4 @@
-// import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
-// import { FaShoppingCart, FaUser } from 'react-icons/fa';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useNavigate, Link } from 'react-router-dom';
-// import { useLogoutMutation } from '../slices/usersApiSlice';
-// import { logout } from '../slices/authSlice';
-// import SearchBox from './SearchBox';
-// import logo from '../assets/logo.png';
-// import { resetCart } from '../slices/cartSlice';
-
-// const Header = () => {
-//   const { cartItems } = useSelector((state) => state.cart);
-//   const { userInfo } = useSelector((state) => state.auth);
-
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const [logoutApiCall] = useLogoutMutation();
-
-//   const logoutHandler = async () => {
-//     try {
-//       await logoutApiCall().unwrap();
-//       dispatch(logout());
-//       // NOTE: here we need to reset cart state for when a user logs out so the next
-//       // user doesn't inherit the previous users cart and shipping
-//       dispatch(resetCart());
-//       navigate('/login');
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   return (
-//     <header>
-//       <Navbar
-//         style={{ backgroundColor: '#242f65' }}
-//         variant='dark'
-//         expand='lg'
-//         collapseOnSelect
-//       >
-//         <Container>
-//           <Navbar.Brand as={Link} to='/'>
-//             {/* <img src={logo} alt='Swaggin' /> */}
-//             <b>Swaggin</b>
-//           </Navbar.Brand>
-//           <Navbar.Toggle aria-controls='basic-navbar-nav' />
-//           <Navbar.Collapse id='basic-navbar-nav'>
-//             <Nav className='ms-auto'>
-//               <SearchBox />
-//               <Nav.Link as={Link} to='/cart' className='white'>
-//                 <FaShoppingCart /> Cart
-//                 {cartItems && cartItems.length > 0 && (
-//                   <Badge pill bg='success' style={{ marginLeft: '5px' }}>
-//                     {cartItems.reduce((a, c) => a + c.qty, 0)}
-//                   </Badge>
-//                 )}
-//               </Nav.Link>
-//               {userInfo ? (
-//                 <>
-//                   <NavDropdown title={userInfo.name} id='username'>
-//                     <NavDropdown.Item as={Link} to='/profile'>
-//                       Profile
-//                     </NavDropdown.Item>
-//                     <NavDropdown.Item onClick={logoutHandler}>
-//                       Logout
-//                     </NavDropdown.Item>
-//                   </NavDropdown>
-//                 </>
-//               ) : (
-//                 <Nav.Link as={Link} to='/login' className='white'>
-//                   <FaUser /> Sign Up
-//                 </Nav.Link>
-//               )}
-
-//               {/* Admin Links */}
-//               {userInfo && userInfo.isAdmin && (
-//                 <NavDropdown title='Admin' id='adminmenu'>
-//                   <NavDropdown.Item as={Link} to='/admin/productlist'>
-//                     Products
-//                   </NavDropdown.Item>
-//                   <NavDropdown.Item as={Link} to='/admin/orderlist'>
-//                     Orders
-//                   </NavDropdown.Item>
-//                   <NavDropdown.Item as={Link} to='/admin/userlist'>
-//                     Users
-//                   </NavDropdown.Item>
-//                 </NavDropdown>
-//               )}
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Container>
-//       </Navbar>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
+import { useState, useRef, useEffect } from 'react';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
@@ -107,6 +10,12 @@ import { resetCart } from '../slices/cartSlice';
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
+  const [adminMenuOpen, setAdminMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const userMenuRef = useRef(null);
+  const adminMenuRef = useRef(null);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -124,6 +33,26 @@ const Header = () => {
     }
   };
 
+  // Handle click outside to close dropdowns
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+        setUserMenuOpen(false);
+      }
+      if (
+        adminMenuRef.current &&
+        !adminMenuRef.current.contains(event.target)
+      ) {
+        setAdminMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className='bg-[#242f65] text-white shadow-md'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6'>
@@ -131,6 +60,7 @@ const Header = () => {
           Swaggin
         </Link>
 
+        {/* Desktop menu */}
         <div className='hidden md:flex items-center space-x-6'>
           <SearchBox />
 
@@ -145,20 +75,37 @@ const Header = () => {
           </Link>
 
           {userInfo ? (
-            <div className='relative group'>
-              <button className='flex items-center space-x-2'>
+            <div ref={userMenuRef} className='relative group'>
+              <button
+                className='flex items-center space-x-2'
+                onClick={() => {
+                  setUserMenuOpen(!userMenuOpen);
+                  setAdminMenuOpen(false);
+                }}
+                onMouseEnter={() => setUserMenuOpen(true)}
+              >
                 <span>{userInfo.name}</span>
                 <FaUser />
               </button>
-              <div className='absolute right-0 mt-2 w-40 bg-white text-gray-900 rounded-md shadow-lg hidden group-hover:block'>
+              <div
+                className={`absolute right-0 mt-2 w-40 bg-white text-gray-900 rounded-md shadow-lg z-10 transition-opacity duration-150 ${
+                  userMenuOpen ? 'opacity-100' : 'opacity-0 invisible'
+                }`}
+                onMouseEnter={() => setUserMenuOpen(true)}
+                onMouseLeave={() => setUserMenuOpen(false)}
+              >
                 <Link
                   to='/profile'
                   className='block px-4 py-2 hover:bg-gray-200'
+                  onClick={() => setUserMenuOpen(false)}
                 >
                   Profile
                 </Link>
                 <button
-                  onClick={logoutHandler}
+                  onClick={() => {
+                    setUserMenuOpen(false);
+                    logoutHandler();
+                  }}
                   className='block w-full text-left px-4 py-2 hover:bg-gray-200'
                 >
                   Logout
@@ -173,26 +120,42 @@ const Header = () => {
           )}
 
           {userInfo?.isAdmin && (
-            <div className='relative group'>
-              <button className='flex items-center space-x-2'>
+            <div ref={adminMenuRef} className='relative group'>
+              <button
+                className='flex items-center space-x-2'
+                onClick={() => {
+                  setAdminMenuOpen(!adminMenuOpen);
+                  setUserMenuOpen(false);
+                }}
+                onMouseEnter={() => setAdminMenuOpen(true)}
+              >
                 <span>Admin</span>
               </button>
-              <div className='absolute right-0 mt-2 w-40 bg-white text-gray-900 rounded-md shadow-lg hidden group-hover:block'>
+              <div
+                className={`absolute right-0 mt-2 w-40 bg-white text-gray-900 rounded-md shadow-lg z-10 transition-opacity duration-150 ${
+                  adminMenuOpen ? 'opacity-100' : 'opacity-0 invisible'
+                }`}
+                onMouseEnter={() => setAdminMenuOpen(true)}
+                onMouseLeave={() => setAdminMenuOpen(false)}
+              >
                 <Link
                   to='/admin/productlist'
                   className='block px-4 py-2 hover:bg-gray-200'
+                  onClick={() => setAdminMenuOpen(false)}
                 >
                   Products
                 </Link>
                 <Link
                   to='/admin/orderlist'
                   className='block px-4 py-2 hover:bg-gray-200'
+                  onClick={() => setAdminMenuOpen(false)}
                 >
                   Orders
                 </Link>
                 <Link
                   to='/admin/userlist'
                   className='block px-4 py-2 hover:bg-gray-200'
+                  onClick={() => setAdminMenuOpen(false)}
                 >
                   Users
                 </Link>
@@ -201,8 +164,99 @@ const Header = () => {
           )}
         </div>
 
-        <button className='md:hidden text-2xl'>&#9776;</button>
+        {/* Mobile menu button */}
+        <button
+          className='md:hidden text-2xl'
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
+          &#9776;
+        </button>
       </div>
+
+      {/* Mobile menu */}
+      {mobileMenuOpen && (
+        <div className='md:hidden bg-[#242f65] px-6 pb-4'>
+          <div className='flex flex-col space-y-4'>
+            <SearchBox />
+
+            <Link
+              to='/cart'
+              className='flex items-center space-x-2'
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <FaShoppingCart />
+              <span>Cart</span>
+              {cartItems?.length > 0 && (
+                <span className='bg-green-500 text-white text-sm font-semibold px-2 py-1 rounded-full'>
+                  {cartItems.reduce((a, c) => a + c.qty, 0)}
+                </span>
+              )}
+            </Link>
+
+            {userInfo ? (
+              <>
+                <Link
+                  to='/profile'
+                  className='flex items-center space-x-2'
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <FaUser />
+                  <span>Profile</span>
+                </Link>
+                <button
+                  onClick={() => {
+                    setMobileMenuOpen(false);
+                    logoutHandler();
+                  }}
+                  className='flex items-center space-x-2'
+                >
+                  <span>Logout</span>
+                </button>
+              </>
+            ) : (
+              <Link
+                to='/login'
+                className='flex items-center space-x-2'
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <FaUser />
+                <span>Sign Up</span>
+              </Link>
+            )}
+
+            {userInfo?.isAdmin && (
+              <>
+                <div className='pt-2 border-t border-gray-700'>
+                  <p className='font-semibold mb-2'>Admin</p>
+                  <div className='flex flex-col space-y-2 pl-4'>
+                    <Link
+                      to='/admin/productlist'
+                      className='block'
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      Products
+                    </Link>
+                    <Link
+                      to='/admin/orderlist'
+                      className='block'
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      Orders
+                    </Link>
+                    <Link
+                      to='/admin/userlist'
+                      className='block'
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      Users
+                    </Link>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </header>
   );
 };
